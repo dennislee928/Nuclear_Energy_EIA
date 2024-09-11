e6cd61ba25485850f59b15c56e84b8dc6a5e6b48
dennislee928 <55692615+dennislee928@users.noreply.github.com>
2024-09-11 11:43:39 +0800

    update: add table

--- front-page/src/components/RetailSales.vue
+++ front-page/src/components/RetailSales.vue
@@ -3,10 +3,18 @@
     <h1>Retail Sales of Electricity</h1>
     <form @submit.prevent="fetchData">
       <label for="state">State ID:</label>
-      <input v-model="stateId" id="state" required />
+      <select v-model="stateId" id="state" required>
+        <option v-for="state in states" :key="state.id" :value="state.id">
+          {{ state.name }}
+        </option>
+      </select>
 
       <label for="sector">Sector ID:</label>
-      <input v-model="sectorId" id="sector" required />
+      <select v-model="sectorId" id="sector" required>
+        <option v-for="sector in sectors" :key="sector.id" :value="sector.id">
+          {{ sector.alias }}
+        </option>
+      </select>
 
       <label for="frequency">Frequency:</label>
       <select v-model="frequency" id="frequency" required>
@@ -20,7 +28,26 @@
 
     <div v-if="data">
       <h2>Results:</h2>
-      <pre>{{ data }}</pre>
+      <table>
+        <thead>
+          <tr>
+            <th>Period</th>
+            <th>State ID</th>
+            <th>State Description</th>
+            <th>Sector ID</th>
+            <th>Sector Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in data.response.data" :key="item.period + item.stateid + item.sectorid">
+            <td>{{ item.period }}</td>
+            <td>{{ item.stateid }}</td>
+            <td>{{ item.stateDescription }}</td>
+            <td>{{ item.sectorid }}</td>
+            <td>{{ item.sectorName }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -34,7 +61,68 @@ export default {
       stateId: '',
       sectorId: '',
       frequency: 'monthly',
-      data: null
+      data: null,
+      sectors: [
+        { id: 'COM', name: 'commercial', alias: '(COM) commercial' },
+        { id: 'RES', name: 'residential', alias: '(RES) residential' },
+        { id: 'ALL', name: 'all sectors', alias: '(ALL) all sectors' },
+        { id: 'OTH', name: 'other', alias: '(OTH) other' },
+        { id: 'TRA', name: 'transportation', alias: '(TRA) transportation' },
+        { id: 'IND', name: 'industrial', alias: '(IND) industrial' }
+      ],
+      states: [
+        { id: 'AL', name: 'Alabama' },
+        { id: 'AK', name: 'Alaska' },
+        { id: 'AZ', name: 'Arizona' },
+        { id: 'AR', name: 'Arkansas' },
+        { id: 'CA', name: 'California' },
+        { id: 'CO', name: 'Colorado' },
+        { id: 'CT', name: 'Connecticut' },
+        { id: 'DE', name: 'Delaware' },
+        { id: 'DC', name: 'District of Columbia' },
+        { id: 'FL', name: 'Florida' },
+        { id: 'GA', name: 'Georgia' },
+        { id: 'HI', name: 'Hawaii' },
+        { id: 'ID', name: 'Idaho' },
+        { id: 'IL', name: 'Illinois' },
+        { id: 'IN', name: 'Indiana' },
+        { id: 'IA', name: 'Iowa' },
+        { id: 'KS', name: 'Kansas' },
+        { id: 'KY', name: 'Kentucky' },
+        { id: 'LA', name: 'Louisiana' },
+        { id: 'ME', name: 'Maine' },
+        { id: 'MD', name: 'Maryland' },
+        { id: 'MA', name: 'Massachusetts' },
+        { id: 'MI', name: 'Michigan' },
+        { id: 'MN', name: 'Minnesota' },
+        { id: 'MS', name: 'Mississippi' },
+        { id: 'MO', name: 'Missouri' },
+        { id: 'MT', name: 'Montana' },
+        { id: 'NE', name: 'Nebraska' },
+        { id: 'NV', name: 'Nevada' },
+        { id: 'NH', name: 'New Hampshire' },
+        { id: 'NJ', name: 'New Jersey' },
+        { id: 'NM', name: 'New Mexico' },
+        { id: 'NY', name: 'New York' },
+        { id: 'NC', name: 'North Carolina' },
+        { id: 'ND', name: 'North Dakota' },
+        { id: 'OH', name: 'Ohio' },
+        { id: 'OK', name: 'Oklahoma' },
+        { id: 'OR', name: 'Oregon' },
+        { id: 'PA', name: 'Pennsylvania' },
+        { id: 'RI', name: 'Rhode Island' },
+        { id: 'SC', name: 'South Carolina' },
+        { id: 'SD', name: 'South Dakota' },
+        { id: 'TN', name: 'Tennessee' },
+        { id: 'TX', name: 'Texas' },
+        { id: 'UT', name: 'Utah' },
+        { id: 'VT', name: 'Vermont' },
+        { id: 'VA', name: 'Virginia' },
+        { id: 'WA', name: 'Washington' },
+        { id: 'WV', name: 'West Virginia' },
+        { id: 'WI', name: 'Wisconsin' },
+        { id: 'WY', name: 'Wyoming' }
+      ]
     }
   },
   methods: {
